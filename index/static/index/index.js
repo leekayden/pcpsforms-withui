85e605aef9113ec01c9452c05895f9a7bc41649c
Kayden Lee <kayden@programmer.net>
2022-05-22 22:04:00 +0800

    update

--- index/static/index/index.js
+++ index/static/index/index.js
@@ -238,3 +238,19 @@ $('.loadingCircle').delay(4500).animate({opacity: '0'}, 500);
 $('body').delay(5000).queue(function(){
 	$('body').addClass("visibleSplash");
 });
+
+window.onscroll = () => {
+  toggleTopButton();
+}
+function scrollToTop(){
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function toggleTopButton() {
+  if (document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20) {
+    document.getElementById('back-to-up').classList.remove('d-none');
+  } else {
+    document.getElementById('back-to-up').classList.add('d-none');
+  }
+}
